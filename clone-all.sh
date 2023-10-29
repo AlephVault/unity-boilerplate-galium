f0a476df50ef2629b838fe70ea7b6d1b83b78364
Luis Masuelli <luis@datagran.io>
2023-10-29 09:16:22 -0500

    Adding the new EVMGames.LocalStorage package to the automatic download

--- clone-all.sh
+++ clone-all.sh
@@ -30,4 +30,4 @@ git clone --branch 0.0.1 git@github.com:AlephVault/unity-ipfs.git ../unity-ipfs
 git clone --branch 0.0.2 git@github.com:AlephVault/unity-evmgames.git ../unity-evmgames
 git clone --branch 0.0.3 git@github.com:AlephVault/unity-evmgames-auth.git ../unity-evmgames-auth
 git clone --branch 0.0.2 git@github.com:AlephVault/unity-evmgames-livecache.git ../unity-evmgames-livecache
-
+git clone --branch 0.0.1 git@github.com:AlephVault/unity-evmgames-localstorage.git ../unity-evmgames-localstorage
